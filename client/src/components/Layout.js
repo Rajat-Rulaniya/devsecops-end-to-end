5e79b6ff52a4ae796e90cb0569e93e8b6be23ca9
Rajat-Rulaniya <rajatrulaniya2005@gmail.com>
2026-01-24 17:27:20 +0530

    Sample code change for testing

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -16,7 +16,7 @@ function Layout({ children }) {
           </div>
         </div>
       </header>
-      <AnimatedBanner message="Welcome to Rajat Rulaniya's DevOps Project 🚀" />
+      <AnimatedBanner message="Welcome to Rajat Rulaniya's DevOps Project (New Version!! 🚀)" />
       <div className="app-body">
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
